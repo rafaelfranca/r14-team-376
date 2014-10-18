b2d25252aa58b0e0672c3f4f68cc577ee20b52f4
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2014-10-18 03:32:24 -0300

    Add test to when the provider and uid already exists

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,5 +18,24 @@ RSpec.describe User, type: :model do
       expect(user.email).to eq('wesleysafadao@garotasafada.com')
       expect(user.image).to eq('image.png')
     end
+
+    it 'does not create a new user if the uid and provider already exists' do
+      User.create!(provider: 'GitHub', uid: '1', email: 'foo@bar.com', password: '12345678')
+
+      info = double('info', name: 'Wesley Safadao', email: 'wesleysafadao@garotasafada.com', image: 'image.png')
+      auth = double('auth', provider: 'GitHub', uid: '1', info: info)
+
+      expect {
+        User.first_or_create_from_github(auth)
+      }.not_to change { User.count }
+
+      user = User.last
+
+      expect(user.provider).to eq('GitHub')
+      expect(user.uid).to eq('1')
+      expect(user.name).to be_nil
+      expect(user.email).to eq('foo@bar.com')
+      expect(user.image).to be_nil
+    end
   end
 end

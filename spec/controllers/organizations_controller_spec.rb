de8a54857afe5e3ecfbccb40a50de47b5afc86bc
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2014-10-18 17:51:37 -0300

    Fix test description

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :controller do
   describe 'POST create' do
-    it 'redirects to root_url if organization can be create' do
+    it 'redirects to new_users_url if organization can be create' do
       post :create, organization: { name: 'Garota Safada' }
 
       organization = Organization.last

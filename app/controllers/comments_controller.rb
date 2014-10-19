36ac9ed970006a2a038f88ddb7e848446df86134
Rondy Sousa <rondy.sousa@plataformatec.com.br>
2014-10-19 19:04:11 -0200

    Update nested route

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,11 @@
 class CommentsController < ApplicationController
   def create
+    # TODO: scope by position?
     step = RecruitmentStep.find(params[:recruitment_step_id])
 
     comment_params = params.require(:comment).permit(:body).merge(author_id: current_user.id)
     step.comments.create(comment_params)
 
-    redirect_to recruitment_recruitment_step_url(step.recruitment, step)
+    redirect_to position_recruitment_recruitment_step_path(step.recruitment.position, step.recruitment, step)
   end
 end
